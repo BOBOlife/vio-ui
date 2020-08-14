20ec5f3ceb174390f32d917e00b5e0117a36e2c4
BOBOWang <bbw1996@qq.com>
2020-08-14 16:09:02 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+ <div>vite</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
